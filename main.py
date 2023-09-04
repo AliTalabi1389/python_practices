a24ff315ac0286066d582c4a277f44d8252d9a77
A.talabi <ali.talabi.25@gmail.com>
2023-09-04 19:31:22 +0430

    add command main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+""" python practice by A.Talabi """
+
 code = "India Romeo Alpha November - Alpha Bravo Alpha Delta"
 words = code.split()
 msg = ""
+
+print(words)
 
 for item in words:
     msg += item[0]
