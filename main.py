a62ba46f3d06afbca4d46ac6fdefb7a56cbaaa3a
A.talabi <ali.talabi.25@gmail.com>
2023-09-04 19:41:49 +0430

    add lines Welcome and End

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """ python practice by A.Talabi """
 
+print("Welcome")
+
 code = "India Romeo Alpha November - Alpha Bravo Alpha Delta"
 words = code.split()
 msg = ""
@@ -8,3 +10,4 @@ for item in words:
     msg += item[0]
 
 print(msg)
+print("End")
